d177f4446a4cd2665498a24b163a670542e8df70
surendar rathor <surendrarathor2003@gmail.com>
2026-03-07 07:02:34 +0530

    two way

--- API/liveAPI.js
+++ API/liveAPI.js
@@ -1,3 +1,4 @@
 const res=await fetch('https://catfact.ninja/fact');
 const data=await res.json();
-console.log(data.fact);
+console.log(data);// i thinks this is give us a oject of object types senario or information 
+console.log(data.fact);// this is also give me information like a set of strings 

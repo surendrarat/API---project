fd88a21496eaed0ecf0bf95aff89c0c064c4841b
surendar rathor <surendrarathor2003@gmail.com>
2026-03-13 19:42:26 +0530

    this is for json response

---
+++ API/JSON.js
@@ -0,0 +1,11 @@
+ // i write some JSON response form server 
+
+ {
+    "name":"suru" , "id" : 1, "isActive": true,"email":"suru@gmail.com",
+    "address": {"city" : "Gwenborough", "zipcode":"3424-323"
+    },
+    "posts":[{"title":"myfirat post"},{"titie":"second post"}]
+
+ }
+ // i know that it is show muptiple ; semicolon error but this is the example of how JSON provide data
+ 
